ac237d338c62ae3076b47ee469de58f28e8a7759
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-12-16 17:35:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from pyrogram.errors import FloodWait
 import time
 
 # Replace these with your own API details
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-SESSION_NAME = "my_userbot"  # Can be any session name
+API_ID = "22359038"
+API_HASH = "b3901895dc193c30c808ba4f1b550ed0"
+SESSION_NAME = "BQFVK_4AOurrGFrrXiK4rFj1YIZD5YhVPy4dA9K_rEmi8v62h7yJaQ26TVlF9alKRhWnrDMdU7az6DTlEaN8JpYkKq-E55gHfe2P12AiMG_pHYiPaHya-Ixd_qdDd87ikfn5ciJH4T3kKDJLiYvoLqI40Oat_UvLJVXNVp1Y4b8_BJ2egPDh5ImiiaRRkBJttuk-St85pYqFZ63aN3-yzYif2Uk1Xxa_J6g0MsY7uSk9gporRmS85MJVwzZJVZ_mYJC94Isr9l6zOGTsikNhkk_o7XziPbaCR4gTE1lm-RYtKZYp98uS0aQT3Q9ZZPuERh0av_qLcadA_DoGmEcQYLi70NToBwAAAAFJs2jlAA"  # Can be any session name
 
 # Initialize the Pyrogram client
 app = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH)
